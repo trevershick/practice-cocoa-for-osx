7d09cbab7d7c56f0b09c3b5d7cd97cd1042f3b52
Trever Shick <tshick@overstock.com>
2016-10-17 16:27:56 -0400

    Dice - Add keyboard handling and focus rings

--- Dice/Dice/DieView.swift
+++ Dice/Dice/DieView.swift
@@ -23,7 +23,16 @@ class DieView : NSView {
             needsDisplay = true
         }
     }
+    dynamic var pressed = false {
+        didSet {
+            needsDisplay = true
+        }
+    }
     
+    
+    func randomize() {
+        intValue = Int(arc4random_uniform(6) + 1)
+    }
 
     override func draw(_ dirtyRect: NSRect) {
         let bg = NSColor.lightGray
@@ -35,25 +44,26 @@ class DieView : NSView {
     }
     
     override var intrinsicContentSize: NSSize {
-        return NSSize(width:50, height:50)
+        return NSSize(width:100, height:100)
     }
     
     func drawDieWith(size s : NSSize) {
-        let (edgeLength, frame) = metricsForSize(size: s)
+        let (edgeLength, f) = metricsForSize(size: s)
         let cornerRadius = edgeLength / CGFloat(cornerRadiusFactor)
         
         let dotRadius = edgeLength / 12.0
-        let dotFrame = frame.insetBy(dx: dotRadius * 2.5, dy: dotRadius * 2.5)
+        let dotFrame = f.insetBy(dx: dotRadius * 2.5, dy: dotRadius * 2.5)
         
         NSGraphicsContext.saveGraphicsState()
-        let shadow = NSShadow()
-        shadow.shadowOffset = NSSize(width:0, height:-1)
-        shadow.shadowBlurRadius = edgeLength/20
-        shadow.set()
-        
+        if pressed == false {
+            let shadow = NSShadow()
+            shadow.shadowOffset = NSSize(width:0, height:-1)
+            shadow.shadowBlurRadius = edgeLength/20
+            shadow.set()
+        }
         
         NSColor.white.set()
-        NSBezierPath(roundedRect: frame, xRadius: cornerRadius, yRadius: cornerRadius).fill()
+        NSBezierPath(roundedRect: f, xRadius: cornerRadius, yRadius: cornerRadius).fill()
         NSGraphicsContext.restoreGraphicsState()
         
         // draw dots
@@ -67,23 +77,21 @@ class DieView : NSView {
             NSBezierPath(ovalIn: dotRect).fill()
         }
         
-        if (1..<7).index(of: intValue) != nil {
-            if [1,3,5].index(of: intValue) != nil {
-                drawDot(u: 0.5,v: 0.5)
-            }
-            if (2..<7).index(of:intValue) != nil {
-                drawDot(u: 0, v: 1)
-                drawDot(u: 1, v: 0)
-                
-            }
-            if (4..<7).index(of:intValue) != nil {
-                drawDot(u: 1, v: 1)
-                drawDot(u: 0, v: 0)
-            }
-            if 6 == intValue {
-                drawDot(u: 0, v: 0.5)
-                drawDot(u: 1, v: 0.5)
-            }
+        if [1,3,5].contains(intValue) {
+            drawDot(u: 0.5,v: 0.5)
+        }
+        if (2..<7).contains(intValue) {
+            drawDot(u: 0, v: 1)
+            drawDot(u: 1, v: 0)
+            
+        }
+        if (4..<7).contains(intValue) {
+            drawDot(u: 1, v: 1)
+            drawDot(u: 0, v: 0)
+        }
+        if 6 == intValue {
+            drawDot(u: 0, v: 0.5)
+            drawDot(u: 1, v: 0.5)
         }
         
     }
@@ -93,7 +101,97 @@ class DieView : NSView {
             let edgeLength = min(size.width, size.height)
             let padding = CGFloat(self.padding)
             let drawingBounds = NSRect(x: 0, y: 0, width: edgeLength, height : edgeLength)
-            let dieFrame = drawingBounds.insetBy(dx: padding, dy: padding)
+            var dieFrame = drawingBounds.insetBy(dx: padding, dy: padding)
+            
+            if pressed {
+                dieFrame = dieFrame.offsetBy(dx: padding/4, dy: -padding/4)
+            }
             return (edgeLength, dieFrame)
+    }
+    
+    func isInDieFrame(_ p : NSPoint) -> Bool {
+        let converted = convert(p, from: nil)
+        let (edgeLength, dieFrame) = metricsForSize(size: bounds.size)
+        
+        let cornerRadius = edgeLength / CGFloat(cornerRadiusFactor)
+        
+        return NSBezierPath(roundedRect: dieFrame, xRadius: cornerRadius, yRadius: cornerRadius).contains(converted)
+    }
+    
+    override func mouseUp(with event: NSEvent) {
+        NSLog("mouseUp \(event)")
+        pressed = false
+        if isInDieFrame(event.locationInWindow) && event.clickCount > 1 {
+            randomize()
+        }
+    }
+    
+    override func mouseDown(with event: NSEvent) {
+        if isInDieFrame(event.locationInWindow) {
+            pressed = true
+        }
+    }
+    override func mouseDragged(with event: NSEvent) {
+        NSLog("mouseDragged \(event)")
+    }
+    
+    override var acceptsFirstResponder: Bool {
+        return true
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    override func resignFirstResponder() -> Bool {
+        return true
+    }
+    
+    // call interpret key events for default behavior
+    override func keyDown(with event: NSEvent) {
+        interpretKeyEvents([event])
+    }
+
+    override func drawFocusRingMask() {
+        NSBezierPath.fill(bounds)
+    }
+    // called when someone hits 'tab'
+    override func insertTab(_ sender: Any?) {
+        window?.selectNextKeyView(sender)
+    }
+    
+    // ... shift tab
+    override func insertBacktab(_ sender: Any?) {
+        window?.selectPreviousKeyView(sender)
+    }
+    
+    override var focusRingMaskBounds: NSRect {
+        return bounds
+    }
+    
+    // called when added to window hierarchy
+    override func viewDidMoveToWindow() {
+        window?.acceptsMouseMovedEvents = true
+        
+        let options = NSTrackingAreaOptions.mouseEnteredAndExited.union(NSTrackingAreaOptions.activeAlways).union(NSTrackingAreaOptions.inVisibleRect)
+        let trackingArea = NSTrackingArea(rect: NSRect(),
+                                          options: options,
+                                          owner: self,
+                                          userInfo: [:])
+        addTrackingArea(trackingArea)
+    }
+    
+    // request focus when the mouse rolls over.
+    override func mouseEntered(with event: NSEvent) {
+        window?.makeFirstResponder(self)
+    }
+    
+    // this is called by NSControler after interpreting key events
+    override func insertText(_ insertString: Any) {
+        if let num : Int = Int(insertString as! String) {
+            if (1..<7).contains(num) {
+            intValue = num
+            }
+            
+        }
     }
 }
